f75af27b1745b9378235650d0104fe268e19b112
Julian St John <julianstjohn@gmail.com>
2022-09-10 16:11:53 +1000

    Fixed: uninstalled cucumber and installed @cucumber/cucumber. Installed pretty-formatter for cuke runs.

--- features/support/challenges.steps.js
+++ features/support/challenges.steps.js
@@ -1,12 +1,12 @@
 const assert = require('assert')
-const { When, Then } = require('cucumber')
+const { When, Then } = require('@cucumber/cucumber')
 const Challenges = require('../../shared/models/challenges.ep.js');
 
 //require('../../shared/models/challenges.ep.js');
 //require('./challenges.ep.js');
 //import { Challenges } from '../../shared/models/challenges.ep.js';
 
-When('an empty POST request is made to /challenger', () => {
+When('an empty POST request is made to \\/challenger', () => {
 })
 
 Then('the header \'x-challenger\' is returned in the response', () => {
