e20fa0285a16e359b7dbaf6458feb87ee0a5ca05
Julian St John <julianstjohn@gmail.com>
2022-09-24 11:14:48 +1000

    Testing Sessions: remaining 3 testing sessions - MIME type and Authorization

--- exploratory/todo.js
+++ exploratory/todo.js
@@ -3,57 +3,57 @@ import request from 'superagent'
 import CookieJar from '../shared/cookiejar.js'
 import { prettyPrint } from '@base2/pretty-print-object';
 import config from '../config.js'
+import fs from 'fs';
+
+
+
+
 
 const cookieJar = new CookieJar(); 
 
-const getTodos = () => {
-    request.get(`${config.apiUrl}/todos`).then((response) => { 
-        //console.log(JSON.stringify(response)
-        console.log(response.text)
-    }).catch(console.log)
+// For a failure, the object has two keys: status, response
+    // response has keys including: res, req, text, buffered, header, headers, error, _body, statusCode
+
+const logSuperAgentRequestOutput =  (superAgentResultObject) => {
+    console.log(JSON.stringify(unbufferTextField(revealSuperagentResponse(superAgentResultObject)))) 
 }
 
+const requestSuccess = logSuperAgentRequestOutput
+
+const requestFailure = logSuperAgentRequestOutput
+
+const revealSuperagentResponse = (superagentRequestOutput) => {
+    return  (superagentRequestOutput.response === undefined) ?  
+    superagentRequestOutput : 
+    superagentRequestOutput.response
+}
+
+const unbufferTextField = (superagentResponse) => {
+ 
+    if( !(superagentResponse.header === undefined)
+        && !(superagentResponse.header['content-type'] === undefined)
+        && superagentResponse.header['content-type'] == 'application/xml'
+        && !(superagentResponse._body === undefined) 
+    ){
+        const buf = Buffer.from(superagentResponse._body)
+        superagentResponse['text'] = buf.toString('utf8')
+    }
+    return superagentResponse
+}
+
+const getTodos = (headers={}) => {
+    request.get(`${config.apiUrl}/todos`)
+    .set(headers)
+    .then(requestSuccess,requestFailure)
+}
 
 const getTodo = () => {
-    request.get(`${config.apiUrl}/todo`).then((response) => { 
-        //console.log(JSON.stringify(response))
-        console.log(response.text)
-    }).catch((error) => {
-        //console.log(error.name)
-        if(error.message == "Not Found"){
-            console.log(JSON.stringify(error)) 
-        }
-    })
+    request.get(`${config.apiUrl}/todo`)
+    .then(requestSuccess,requestFailure)  
 }
 
 const getTodosWithId = (id) => {
-    request.get(`${config.apiUrl}/todos/${id}`).then((response) => { 
-        console.log(JSON.stringify(response))
-        //console.log(response.text)
-    }).catch((error) => {
-        //console.log(error.name)
-        if(error.message == "Not Found"){
-            console.log(JSON.stringify(error)) 
-        }
-    })
-}
-
-//getTodos()
-//getTodosWithId(4)
-//getTodosWithId(-1)
-
-const postTodos = (sendObj) => {
-    request.post(`${config.apiUrl}/todos`)
-        .send(sendObj)
-        .then(
-            (response) => { 
-            console.log(JSON.stringify(response))
-            //console.log(response.text)
-        },
-            (error) => {
-            //console.log(error.name)
-            console.log(JSON.stringify(error)) 
-        })
+    request.get(`${config.apiUrl}/todos/${id}`).then(requestSuccess,requestFailure)  
 }
 
 // Test that we are prevented from specifying an id when creating a record
@@ -61,58 +61,53 @@ const postTodos = (sendObj) => {
 //let sendObj = {'title':'ggoK01DZCweTwYOMzZQbOZuChWG6BW2ogGnrpHIMbHU8ChSs9M6rf7hdEvx4AOV9NK2whltrjLE3SKqytxMkgIxUrBfttONmGEXtpajUFvSrQt1lixqhTwC9IQbrPqTIaQCCXD4qMRXFxnx4sOxKn0AO6VSGOCuEHcSIO5hdjHaaO0GIh45W6ynv53A0nxBTqP929aQB'}
 //postTodos(sendObj);
 
+const postTodos = (dataString, headers={}) => {
+    request.post(`${config.apiUrl}/todos`)
+        .set(headers)
+        .send(dataString)
+        .then(requestSuccess)
+        .catch(requestFailure)
 
+}
 
 
 const optionsTodos = () => {
-    request.options(`${config.apiUrl}/todos`)
-        .then((response) => { console.log(JSON.stringify(response)) },
-              (error) => { console.log(JSON.stringify(error)) })
+    request.options(`${config.apiUrl}/todos`) .then(requestSuccess,requestFailure)  
 }
-
-//optionsTodos();
 
 const headTodosWithId = (id) => {
-    request.head(`${config.apiUrl}/todos/${id}`)
-    .then(  (response) => { console.log(JSON.stringify(response)) },
-            (error) => { console.log(JSON.stringify(error)) })
+    request.head(`${config.apiUrl}/todos/${id}`) .then(requestSuccess,requestFailure)  
 }
-//headTodosWithId(1)
 
 const headTodos = () => {
-    request.head(`${config.apiUrl}/todos`)
-    .then(  (response) => { console.log(JSON.stringify(response)) },
-            (error) => { console.log(JSON.stringify(error)) })
+    request.head(`${config.apiUrl}/todos`) .then(requestSuccess,requestFailure)  
 }
 
 const deleteWithId = (id) => {
-    request.delete(`${config.apiUrl}/todos/${id}`)
-    .then(  (response) => { console.log(JSON.stringify(response)) },
-            (error) => { console.log(JSON.stringify(error)) })
+    request.delete(`${config.apiUrl}/todos/${id}`).then(requestSuccess,requestFailure)  
 }
 
-const update = (id, sendObj) => {
+const update = (id, dataString, headers={}) => {
     request.post(`${config.apiUrl}/todos/${id}`)
-        .send(sendObj)
-        .then(  (response) => { console.log(JSON.stringify(response)) },
-                (error) => { console.log(JSON.stringify(error)) })
+        .set(headers)
+        .send(dataString)
+        .then( requestSuccess, requestFailure)
 }
 
 const getFilteredtodos = () => {
-    request.get(`${config.apiUrl}/todos?doneStatus=true`)
-    .then(  (response) => { console.log(JSON.stringify(response)) },
-            (error) => { console.log(JSON.stringify(error)) })
+    request.get(`${config.apiUrl}/todos?doneStatus=true`).then(requestSuccess,requestFailure)  
 }
  
 
+// application/json, application/xml, application/gzip, */*, ;q=0.8
+
 // can't post with donestatus of true
 let sendObj = {
-                //'title':'this is still x23', 
-                //'description':'testing for skipped ',
+                'title':'initial upddate', 
+                'description':'first check for post in string',
                 'doneStatus': true
             }
 //postTodos(sendObj);
-//getTodosWithId('17')
 //deleteWithId(15)
 
 //update(10, sendObj)
@@ -125,6 +120,64 @@ let sendObj = {
 
 //postTodos(sendObj)
 //getTodos()
-//getTodosWithId(23)
+//getTodosWithId(99)
 
-getFilteredtodos()
+//getFilteredtodos()
+
+//getTodos('application/xml, application/json')
+//getTodos('application/xml')
+//console.log(JSON.stringify(sendObj))
+//update('1',JSON.stringify(sendObj))
+let jsonUpdateData = JSON.stringify(sendObj)
+
+let xmlUpdateData = "<title>Created with XML</title>"
+//xmlUpdateData += "<id>1</id>"
+xmlUpdateData += "<description>Pog</description>"
+xmlUpdateData += "<doneStatus>true</doneStatus>"
+xmlUpdateData = "<todo>" + xmlUpdateData + "</todo>"
+
+// Create a tood item:
+//postTodos(jsonUpdateData, {'Content-Type':'application/json','Accept':'application/xml'})
+//postTodos(jsonUpdateData, {'Content-Type':'application/json','Accept':'application/json'})
+
+//update(1,xmlUpdateData, {'Content-Type':'application/xml','Accept':'application/xml'})
+
+const authheadersWithCorrectUidPassword = {'Authorization': 'Basic ' + (new Buffer.from("admin:password").toString('base64'))}
+const authheadersWithIncorrectUidPassword = {'Authorization': 'Basic ' + (new Buffer.from("admin:pwd").toString('base64'))}
+const authheadersWithoutUidPassword = {'Authorization': 'Basic' }
+
+// Get credentials: 
+//request.post(`${config.apiUrl}/secret/token`).set(authheadersWithCorrectUidPassword).then( requestSuccess, requestFailure)
+
+const authHeader = {'x-auth-token':'a350bc9b-9f65-4c5e-b6a6-d56e21a82512'}
+const authHeaderAcceptText = authHeader
+authHeaderAcceptText['accept'] = 'application/ggg'
+const bearerAuthHeader = {'Authorization':'Bearer a350bc9b-9f65-4c5e-b6a6-d56e21a82512'}
+const secretNoteUrl = `${config.apiUrl}/secret/note`
+const incorrectAuthHeader = {'x-auth-token':'bananas'}
+//request.get(`${config.apiUrl}/secret/note`).set(bearerAuthHeader).then(logSuperAgentRequestOutput,logSuperAgentRequestOutput)
+
+const noteData = {"note":"Here is a third note"}
+//request.post(secretNoteUrl).set(bearerAuthHeader).send(noteData).then( requestSuccess, requestFailure)
+
+//getTodosWithId('1')
+
+// other methods:
+//request.delete(`${config.apiUrl}/heartbeat`).then( requestSuccess, requestFailure)
+
+
+// postTodos("<todo><title>highere</title></todo>",{'content-type':'application/xml','accept':'blahino' })
+
+getTodos({'accept':'*/*' })
+
+// delete every todo: 
+
+/*
+request.get(`${config.apiUrl}/todos`).then(
+    (response) => {JSON.parse(response.text).todos.forEach((todo) => { 
+       let id = todo['id']
+       request.delete(`${config.apiUrl}/todos/${id}`).then(requestSuccess,requestFailure)  
+    }
+    )}, 
+    requestFailure)
+*/
--- notes/testing-notes.md
+++ notes/testing-notes.md
@@ -182,12 +182,12 @@ _Update_
 ## Issues
 
 --- 
-## Charter:
+## Charter: Test MIME type of sent / accepted data on /todos endpoint 
 
 ### Session Details
-* **Date:** 
-* **Start:**  
-* **Duration:** 
+* **Date:** 17/9/22
+* **Start:**  12.45pm
+* **Duration:** 45m
 * **Tester:** Julian
 
 ### Tools
@@ -196,6 +196,114 @@ _Update_
 
 ### Test Notes 
 
+_Updating functions to accept application/xml_ 
+
+* Sending 'Accept: application/xml', I found that xml is buffered in superagent - will need to be rendered as string
+* Went down a wrong path trying to call `.buffer(false)` - I'm not sure what this does in superagent. 
+* modified getTodos to unbuffer the body and set it to string where xml is received
+* sampled the data returned for /todos between xml/json to check order and details are the same 
+* The input I've used to superagent to the POST requests has been a js object, which superagent converts to json - I'm going to try using a string
+    * this is a notable point in testing tools - examining what the tool is actually doing
+    * I am assuming that superagent accepts a js object, stringifies it, and sends the string
+    * going to try using a string instead of js object, see if there is any difference in behaivour.  
+    * called JSON.stringify on data being sent, no difference in results. 
+* Refactoring: separating out the logic to log xml into a function. Actually makes the request calls cleaner. 
+
 ### Summary 
 
 ## Issues
+
+* an update (challenges id 20) to /todos/id won't satisfy the condition of this challenge. Not really an issue but would seem to go against the concept of the challenge. 
+* an update (challenge 22) with invalid accept type and content type won't satisfy the challenge - has to be only content type. Not an issue as the http codes are different. (406 vs 415)
+* Okay so I found an interesting one! When I made a mistake with my function call: 
+    * `postTodos(1,xmlUpdateData, {'Content-Type':'application/xml','Accept':'application/xml'})` 
+    * which results in a superagent call of  `request.post(${config.apiUrl}/todos).set("<todos><title ...").send("1")`, giving headers of an xml string and body of "1"
+    * the result I get back is `{"code":"ERR_INVALID_HTTP_TOKEN"}` - unsurprising,
+    * _but the script hangs_. I need to ^C out of it. 
+    * On first inspection I'm thinking that the http error means the connection remains open. 
+* From the documentation, the behaviour of `.then()` might differ between POST / PUT and GET, in terms of error handling. 
+
+--- 
+
+## Charter: Test MIME type of sent / accepted data on /todos endpoint (repeat)
+
+### Session Details
+* **Date:** 19/9/22
+* **Start:** 10.30am 
+* **Duration:** 45m 
+* **Tester:** Julian
+
+### Tools
+
+### Files 
+
+### Test Notes 
+
+_Accept_
+
+* delivers xml when `accept: application/xml` is passed
+* invalid accept string `accept: json` returns a 406
+* delivers json by default
+* `application/gzip` returns a code of 406
+
+_Content-Type_
+
+* Where xml is sent with no content-type header, api returns `Unsupported Content Type - application/x-www-form-urlencoded\` 
+* Where data is submitted in xml, and no accept specified, the new record is returned in json. I think this behaviour should be api-specific. Capture this in the feature file. 
+* Added a basic feature file for this case. 
+* `application/gzip` is unsupported, returns a status of 415
+* content type of any `*/*` results in json being sent
+* content-type and data of xml, accept of `*/*` results in json being returned
+
+
+
+### Summary 
+
+* Content Type and Accept headers work as expected
+
+
+## Issues
+
+--- 
+## Charter: Authorisation - /secret/token and /secret/note endpoints
+
+### Session Details
+* **Date:** 23/9/22
+* **Start:** 4.15pm 
+* **Duration:** 45m 
+* **Tester:** Julian
+
+### Tools
+
+### Files 
+
+### Test Notes 
+
+_Auth token: /secret/token_
+
+* Token is retrievable with correct uid/password: ` "x-auth-token": "a350bc9b-9f65-4c5e-b6a6-d56e21a82512",`
+* Token should not be retrievable without uid/password: 401
+* Token should not be retrievable without wrong password: 401
+* Token is not retrievable with no `Authorization` header: 401
+* Using POST: ok
+* Using GET: 405 (Method not allowed)
+* Multiple requests seem to get the same token `a350bc9b-9f65-4c5e-b6a6-d56e21a82512` 
+    * could be time limited?
+
+_Secure resource: /secret/note_
+
+* Token is correct - 200, text of `{\"note\":\"\"}` is returned. 
+* when accept: application/xml is passed, it is ignored
+* Token is incorrect: 403
+* No token: 401
+* POST a note returns a 200 with correct x-auth-token header
+* It seems only one note at a time is captured to be returned with the next get
+* Can't POST a note with incorrect or missing x-auth-token
+* Alternative header `authorization: Bearer <token string>` is accepted
+
+### Summary 
+
+* Secure resource /secret/note is correctly protected by authorization 
+
+## Issues
+* `Accept` is ignored for /secret/note, even for invalid values. 
